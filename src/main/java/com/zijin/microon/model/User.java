94dd550dfe963ecbbce03772ef6d442d44521b22
李爱东 <1158889685@qq.com>
2015-07-24 15:14:38 +0800

    初始化

---
+++ src/main/java/com/zijin/microon/model/User.java
@@ -0,0 +1,43 @@
+package com.zijin.microon.model;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer score;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
