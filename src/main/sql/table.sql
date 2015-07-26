a10d7a842a6bf421e1f8970a020474bfe44071bc
李爱东 <1158889685@qq.com>
2015-07-26 09:57:26 +0800

    脚本

--- src/main/sql/table.sql
+++ src/main/sql/table.sql
@@ -6,6 +6,7 @@ create table if not exists user (
 id int(11) NOT NULL auto_increment,
 name varchar(20),
 password varchar(50),
+email varchar(50),
 birthday date,
 primary key(id)
 );
@@ -15,6 +16,17 @@ create table if not exists article (
 id int(11) NOT NULL auto_increment,
 title varchar(80),
 content MEDIUMTEXT,
-issueDate date,
+issueDate date,  #发布时间
+authorNo 
+primary key(id)
+);
+
+#文章修改历史  文章修改前，对文章内容进行全量保存
+create table if not exists article (
+id int(11) NOT NULL auto_increment,
+title varchar(80),
+content MEDIUMTEXT,
+reviseDate date,  #修改时间
+modifierNo int,   #文章修改人  外键
 primary key(id)
 );
