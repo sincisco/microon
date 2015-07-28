d77562f52793f38de3759d7e79b60d53c36992ea
李爱东 <1158889685@qq.com>
2015-07-28 14:44:17 +0800

    添加文章分类表

--- src/main/sql/table.sql
+++ src/main/sql/table.sql
@@ -15,9 +15,10 @@ primary key(id)
 create table if not exists article (
 id int(11) NOT NULL auto_increment,
 title varchar(80),
+outline TEXT,
 content MEDIUMTEXT,
 issueDate date,  #发布时间
-authorNo 
+authorNo int,
 primary key(id)
 );
 
@@ -28,5 +29,15 @@ title varchar(80),
 content MEDIUMTEXT,
 reviseDate date,  #修改时间
 modifierNo int,   #文章修改人  外键
+primary key(id)
+);
+
+#文章分类
+create table if not exists articleCategory (
+id int(11) NOT NULL auto_increment,
+name varchar(80) NOT NULL,  
+parentId int(11),	  #父分类
+createDateTime datetime,  #创建时间or修改时间
+creator int,   #文章分类创建者  外键
 primary key(id)
 );
